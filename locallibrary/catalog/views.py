2d29e42ba16e8b1ab51e6ef1d7ce993e16f15562
gr434_mueva <egormyrt21@gmail.com>
2025-11-25 14:00:45 +0700

    create home page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
+    num_genres_fantasy = Genre.objects.filter(name='fantasy').count()
 
     return render(
         request,
         'index.html',
         context={'num_books':num_books,
-                 'num_instances':num_instances, 'num_instances_available':num_instances_available, 'num_authors':num_authors},
+                 'num_instances':num_instances,
+                 'num_instances_available':num_instances_available,
+                 'num_authors':num_authors,
+                 'num_genres_fantasy':num_genres_fantasy},
     )
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
 
 
