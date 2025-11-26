0b67a8e27d9ea6d27585c73d6c3e9da8e4df6107
gr434_mueva <egormyrt21@gmail.com>
2025-11-26 11:46:31 +0700

    commit login

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -9,20 +9,34 @@ def index(request):
     num_authors = Author.objects.count()
     num_genres_fantasy = Genre.objects.filter(name='fantasy').count()
 
-    return render(
-        request,
-        'index.html',
-        context={'num_books':num_books,
-                 'num_instances':num_instances,
-                 'num_instances_available':num_instances_available,
-                 'num_authors':num_authors,
-                 'num_genres_fantasy':num_genres_fantasy},
-    )
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
+    context = {'num_books': num_books,
+               'num_instances': num_instances,
+               'num_instances_available': num_instances_available,
+               'num_authors': num_authors,
+               'num_genres_fantasy': num_genres_fantasy,
+               'num_visits': num_visits,
+               }
+
+    return render(request, 'index.html', context=context)
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 2
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+
 
 
